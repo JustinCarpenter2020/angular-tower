f994c70c415b80db62df91263f7864470b3dcd23
justin <JustinCarpenter2020@Gmail.com>
2022-01-25 15:13:30 -0700

    id added to event details

--- src/app/pages/event-details/event-details.component.ts
+++ src/app/pages/event-details/event-details.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
-  // @Input() queryParams?: Params | null
-  constructor() { }
+  private sub: any;
+  id: string = '';
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = params['id']
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
